ac67d763fbaed2575a0cee5c11c4f45b2123801d
José Manuel Reyes Jiménez <reyes.jijos20@cadiz.salesianos.edu>
2023-11-01 18:08:59 +0100

    update pictures, power, root, factorial

--- trabajora1.py
+++ trabajora1.py
@@ -1,8 +1,16 @@
 import shutil
-from multiprocessing import Process
+from multiprocessing import Process, Value, Pool
 import multiprocessing
-from multiprocessing import Value
+from PIL import Image, ImageEnhance, ImageFilter
+import time
+import numpy as np
+
+
 resultado_compartido = Value('i', 0)
+listEscalado = ["imagem_2023_04_30_22184991439.jpg"]
+listNitidez = ['']
+listConstraste = ['']
+listRotacion = ['']
 
 def sumar(x,y,resultado):
     resultado.value = x + y
@@ -15,22 +23,86 @@ def mul(x,y, resultado):
 
 def div(x,y, resultado ):
     resultado.value= x//y
+    
+def invert_color(pixel):
+    r, g, b = pixel
+    return 255 - r, 255 - g, 255 - b
+
+def process_image(image_path):
+    # Cargar la imagen
+    image = Image.open("camaro.jpg")
+    image = np.array(image)
+
+    # Crear un objeto Pool
+    pool = Pool()
+
+    # Aplicar el filtro de inversión de color a cada pixel de la imagen
+    result = pool.map(invert_color, [pixel for row in image for pixel in row])
+
+    # Reshape del resultado para que tenga la misma estructura que la imagen original
+    result = np.array(result).reshape(image.shape)
+
+    # Convertir el resultado a una imagen y guardarla
+    result_image = Image.fromarray(result.astype('uint8'))
+    result_image.save('imagen_mejorada.jpg')
 
 def is_par(x):
     if x/2==0:
         return True
     else: 
         return False
+    
+def cuadrado(x):
+        print('%s al cuadrado es %s' % (x, int(x)**2))
+
+def raiz (x):
+        print('%s su raiz es %s' % (x, int(x)**0.5))
+        
+def factorial (x):
+        resultado = 1 
+        for i in range(1, int(x) + 1):
+         resultado *= i
+         print('%s su factorial es ' + str(resultado))
+        
+def EscaladoImagen(imagen):
+    img = Image.open(imagen)
+    img.resize((2160,1440))
+    img.save('Escalado' + imagen)
+    listNitidez.append(img)
+    
+def ImagenNitidez(imagen1):
+    img = Image.open(imagen1)
+    imgnitida = img.filter(ImageFilter.SHARPEN)
+    imgnitida.save('Nitidez'+ imagen1)
+    listConstraste.append(imgnitida)
+
+def ConstrasteImagen(imagen2):
+    img = Image.open(imagen2)
+    imgmejorada =ImageEnhance.Contrast(img)
+    imgmejorada2 =imgmejorada.enhance(1.4)
+    imgmejorada2.save('Contraste'+ imagen2)
+    listRotacion.append(imgmejorada2)
+
+def ImagenRotacion(imagen3):
+    img = Image.open(imagen3)
+    img = img.rotate(20)
+    img.save('Final'+ imagen3)
+    img.show()
 
 def mostrar_menu():
     print("1) Operaciones Básicas (+,-,x,/) | esPar? | esPrimo?")
-    print("4) Salir")
+    print("2) Potencia, Raiz, Factorial de una lista de Numeros")
+    print("3) Invertir color pixel de Imagen")
+    print("4) Procesos en Imagenes")
+    print("5) Salir")
 
 def main():
     while True:
         mostrar_menu()
         opcion = input("Elige una opción: ")
         if opcion == "1":
+            numero1=int(input("Introduce un número: "))
+            numero2=int(input("Introduce un número: "))
             p1 = Process(target=sumar, args=(numero1,numero2, resultado_compartido))
             p2 = Process(target=restar, args=(numero1,numero2, resultado_compartido))
             p3 = Process(target=mul, args=(numero1,numero2, resultado_compartido))
@@ -65,12 +137,45 @@ def main():
                 print(row[0], row[1], row[2])
                     
         elif opcion == "2":
-            print("Has seleccionado la Opción 2")
-            # Aquí puedes poner el código relacionado con la Opción 2
+            numeros = [3, 8, 5, 8, 4, 2]
+            for x in numeros:
+             print('    Potencia y Raiz de ', x)
+             p1 = Process(target=cuadrado, args=(str(x)))
+             p2 = Process(target=raiz, args=(str(x)))
+             p3 = Process(target=factorial, args=(str(x)))
+
+             p1.start()
+             p2.start()
+             p3.start()
+
+             p1.join()
+             p2.join()
+             p3.join()
+             
+             print('-------------------------------------------')
         elif opcion == "3":
-            print('4')
+            process_image("camaro.jpg")
         elif opcion == "4":
-            print("Has seleccionado la Opción 4")
+              #Array de varias Imagenes
+              p1 = Process(target=EscaladoImagen, args=("camaro.jpg",))
+              time.sleep(2)
+              p2 = Process(target=ImagenNitidez, args=("Escaladocamaro.jpg",))
+              time.sleep(2)
+              p3 = Process(target=ConstrasteImagen, args=("NitidezEscaladocamaro.jpg",))
+              time.sleep(2)
+              p4 = Process(target=ImagenRotacion, args=("ContrasteNitidezEscaladocamaro.jpg",))
+              
+
+              p1.start()
+              p2.start()
+              p3.start()
+              p4.start()
+
+              p1.join()
+              p2.join()
+              p3.join()
+              p4.join()
+         
         elif opcion == "5": 
             print("FIN DEL PROGRAMA")
             break
@@ -78,6 +183,4 @@ def main():
             print("Opción no válida. Por favor, elige una opción válida.")
 
 if __name__ == "__main__":
-    numero1=int(input("Introduce un número: "))
-    numero2=int(input("Introduce un número: "))
     main()
